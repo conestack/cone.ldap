8690ea5167643806a9b9b918e0fa3bb48c0e4868
Robert Niederreiter <office@squarewave.at>
2019-05-04 11:56:00 +0200

    Remove superfluous -ldap_password_field_factory

--- src/cone/ldap/browser/__init__.py
+++ src/cone/ldap/browser/__init__.py
@@ -1,6 +1,5 @@
 from cone.ugm.browser.principal import default_form_field_factory
 from cone.ugm.browser.principal import email_field_factory
-from cone.ugm.browser.principal import password_field_factory
 from cone.ugm.browser.principal import user_field
 from functools import partial
 from pyramid.static import static_view
@@ -9,9 +8,6 @@ from pyramid.static import static_view
 static_resources = static_view('static', use_subpath=True)
 
 
-ldap_password_field_factory = user_field('userPassword', backend='ldap')(
-    password_field_factory
-)
 ldap_email_field_factory = user_field('mail', backend='ldap')(
     email_field_factory
 )
