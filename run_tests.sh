71b87bdb18467323649e0017f49d5f4d7d466bc0
Robert Niederreiter <office@squarewave.at>
2020-06-01 14:43:11 +0200

    Update testing config. Reduce code in testing module

--- run_tests.sh
+++ run_tests.sh
@@ -7,4 +7,10 @@ export SLAPD_URIS=ldap://127.0.0.1:12345
 export ADDITIONAL_LDIF_LAYERS=
 
 ./bin/py -m cone.ugm.tests.__init__
-./bin/py -m cone.ldap.tests.__init__
+#./bin/py -m cone.ldap.tests.__init__
+
+unset LDAP_ADD_BIN
+unset LDAP_DELETE_BIN
+unset SLAPD_BIN
+unset SLAPD_URIS
+unset ADDITIONAL_LDIF_LAYERS
--- src/cone/ldap/testing/__init__.py
+++ src/cone/ldap/testing/__init__.py
@@ -3,14 +3,15 @@ from cone.app.testing import Security
 from cone.app.ugm import ugm_backend
 from cone.ldap.settings import ldap_cfg
 from cone.ugm.settings import ugm_cfg
-from node.ext.ldap import LDAPNode
 from node.ext.ldap.testing import LDIF_base
+from node.ext.ldap.ugm.defaults import creation_defaults
 from plone.testing import Layer
 import os
 
 
 base_path = os.path.split(__file__)[0]
 ugm_config = os.path.join(base_path, 'ugm.xml')
+localmanager_config = os.path.join(base_path, 'localmanager.xml')
 ldap_server_config = os.path.join(base_path, 'ldap_server.xml')
 ldap_users_config = os.path.join(base_path, 'ldap_users.xml')
 ldap_groups_config = os.path.join(base_path, 'ldap_groups.xml')
@@ -60,61 +61,46 @@ def custom_config_path(fn):
     return wrapper
 
 
+def rdn_value(node, uid):
+    return uid.split('=')[1]
+
+
+def create_mail(node, uid):
+    return '%s@example.com' % rdn_value(node, uid)
+
+
+creation_defaults['inetOrgPerson'] = dict()
+creation_defaults['inetOrgPerson']['sn'] = rdn_value
+creation_defaults['inetOrgPerson']['cn'] = rdn_value
+creation_defaults['inetOrgPerson']['mail'] = create_mail
+
+
 class LDAPLayer(Security, Layer):
     defaultBases = (LDIF_base,)
 
     def __init__(self):
         Layer.__init__(self)
 
-    def tearDown(self):
-        super(LDAPLayer, self).tearDown()
-        self.cleanup_test_principals()
-
     def make_app(self):
         super(LDAPLayer, self).make_app(**{
-            'cone.plugins': 'cone.ldap\ncone.ugm',
+            'cone.plugins': '\n'.join([
+                'cone.ldap',
+                'cone.ugm'
+            ]),
             'ugm.backend': 'ldap',
             'ugm.config': ugm_config,
+            'ugm.localmanager_config': localmanager_config,
             'ldap.server_config': ldap_server_config,
             'ldap.users_config': ldap_users_config,
             'ldap.groups_config': ldap_groups_config,
             'ldap.roles_config': ldap_roles_config
         })
-        self.setup_principal_containers()
-        self.setup_test_principals()
 
-    def setup_principal_containers(self):
         settings = get_root()['settings']
-        server_settings = settings['ldap_server']
-        props = server_settings.ldap_props
-        root = LDAPNode('dc=my-domain,dc=com', props)
-        for rdn in ['ou=users', 'ou=groups', 'ou=roles']:
-            root[rdn] = LDAPNode()
-            root[rdn].attrs['objectClass'] = ['organizationalUnit']
-        root()
-
-    def setup_test_principals(self):
+        settings['ldap_users'].create_container()
+        settings['ldap_groups'].create_container()
+        settings['ldap_roles'].create_container()
         ugm_backend.initialize()
-        ugm = ugm_backend.ugm
-        user_ids = ['viewer', 'editor', 'admin', 'manager']
-        for user_id in user_ids:
-            user = ugm.users.create(
-                user_id,
-                cn=user_id,
-                sn=user_id,
-                mail='{}@example.com'.format(user_id)
-            )
-            user.add_role(user_id)
-        ugm()
-        for user_id in user_ids:
-            ugm.users.passwd(user_id, None, 'secret')
-        ugm()
-
-    def cleanup_test_principals(self):
-        ugm = ugm_backend.ugm
-        for user_id in ['viewer', 'editor', 'admin', 'manager']:
-            del ugm.users[user_id]
-        ugm.users()
 
 
 ldap_layer = LDAPLayer()
