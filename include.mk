9fb43b36e9bd8ce70278feaf4d0395cd4de9fead
Robert Niederreiter <rnix@squarewave.at>
2024-11-20 12:51:14 +0100

    dependency targets

--- include.mk
+++ include.mk
@@ -11,27 +11,27 @@ define set_testldap_env
 endef
 
 .PHONY: start-ldap-base
-start-ldap-base:
+start-ldap-base: $(INSTALL_TARGETS)
 	$(call set_testldap_env)
 	@$(VENV_FOLDER)/bin/testldap start base
 
 .PHONY: start-ldap-gon-10-10
-start-ldap-gon-10-10:
+start-ldap-gon-10-10: $(INSTALL_TARGETS)
 	$(call set_testldap_env)
 	@$(VENV_FOLDER)/bin/testldap start groupOfNames_10_10
 
 .PHONY: start-ldap-gon-100-100
-start-ldap-gon-100-100:
+start-ldap-gon-100-100: $(INSTALL_TARGETS)
 	$(call set_testldap_env)
 	@$(VENV_FOLDER)/bin/testldap start groupOfNames_100_100
 
 .PHONY: start-ldap-posix
-start-ldap-posix:
+start-ldap-posix: $(INSTALL_TARGETS)
 	$(call set_testldap_env)
 	@$(VENV_FOLDER)/bin/testldap start posixGroups
 
 .PHONY: stop-ldap
-stop-ldap:
+stop-ldap: $(INSTALL_TARGETS)
 	$(call set_testldap_env)
 	@$(VENV_FOLDER)/bin/testldap stop
 
@@ -40,17 +40,17 @@ stop-ldap:
 ##############################################################################
 
 .PHONY: run-base
-run-base:
+run-base: $(INSTALL_TARGETS)
 	@$(VENV_FOLDER)/bin/pserve cfg/base/ldap_base.ini
 
 .PHONY: run-gon-10-10
-run-gon-10-10:
+run-gon-10-10: $(INSTALL_TARGETS)
 	@$(VENV_FOLDER)/bin/pserve cfg/gon_10_10/ldap_gon_10_10.ini
 
 .PHONY: run-gon-100-100
-run-gon-100-100:
+run-gon-100-100: $(INSTALL_TARGETS)
 	@$(VENV_FOLDER)/bin/pserve cfg/gon_100_100/ldap_gon_100_100.ini
 
 .PHONY: run-posix
-run-posix:
+run-posix: $(INSTALL_TARGETS)
 	@$(VENV_FOLDER)/bin/pserve cfg/posix/ldap_posix.ini
